67a398a6f97b3b04906d306186b5073f18410fd0
Michel Wilhelm <mwilhelm@collective.com>
2023-01-27 11:30:44 -0300

    chore: hiding debug messages

--- infisical.plugin.zsh
+++ infisical.plugin.zsh
@@ -16,11 +16,6 @@ function cd {
             DEFAULT_ENV=$ZSH_INFISICAL_DEFAULT_ENV
         fi
 
-        # If ZSH_INFISICAL_ENV variable is set
-        if [ -n "$ZSH_INFISICAL_ENV" ]; then
-            DEFAULT_ENV=$ZSH_INFISICAL_ENV
-        fi
-
         # If INFISICAL_API_URL variable is set
         if [ -n "$INFISICAL_API_URL" ]; then
             DEFAULT_URL=$INFISICAL_API_URL
@@ -31,13 +26,10 @@ function cd {
             DEFAULT_URL=$ZSH_INFISICAL_DEFAULT_URL
         fi
 
-        # If ZSH_INFISICAL_URL variable is set
-        if [ -n "$ZSH_INFISICAL_URL" ]; then
-            DEFAULT_URL=$ZSH_INFISICAL_URL
+        # If ZSH_INFISICAL_VERBOSE variable is set
+        if [ -n "$ZSH_INFISICAL_VERBOSE" ]; then
+            VERBOSE=$ZSH_INFISICAL_VERBOSE
         fi
-
-        echo "[zsh-infisical] Using env: $DEFAULT_ENV" >&2
-        echo "[zsh-infisical] Using url: $DEFAULT_URL" >&2
 
         # Activate the environment using the eval command with the CLI output
         infisical --domain=$DEFAULT_URL export -f dotenv-export -e $DEFAULT_ENV | eval
